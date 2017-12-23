f012fee08c09b6c71e6e11c39f3cee4c6267f507
Kais-DkM <kais0903@gmail.com>
2017-12-24 02:47:15 +0900

    add bindings, backup, autosave config

--- init.el
+++ init.el
@@ -20,3 +20,38 @@
 
 (setq package-selected-packages '(req-package evil f s dash))
 (package-install-selected-packages)
+
+
+(require 'f)
+
+(setq init-dir (f-dirname (f-this-file)))
+(load (f-join init-dir "bindings" "init.el"))
+(add-to-list 'load-path (f-join init-dir "scripts"))
+
+(setq gc-cons-threshold 20000000)
+
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; (setq-default tab-always-indent 'complete)
+
+(let ((backup-dir (f-slash (f-join init-dir "backup"))))
+  (unless (f-directory? backup-dir) (f-mkdir backup-dir))
+  (setq backup-directory-alist `((".*" . ,backup-dir))))
+(let ((autosave-dir (f-slash (f-join init-dir "autosave"))))
+  (unless (f-directory? autosave-dir) (f-mkdir autosave-dir))
+  (setq auto-save-file-name-transforms `((".*" ,autosave-dir t)))
+  (setq auto-save-list-file-prefix (f-join autosave-dir ".save-list")))
+(setq backup-by-copying t
+      version-control t
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      )
+
+(setq scroll-step 1)
+(setq frame-resize-pixelwise t)
+
+(setq custom-file (f-join init-dir "custom.el"))
+(load custom-file 'noerror)
