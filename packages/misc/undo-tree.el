fb0894be11244bf6749c45ff11f6a9f8bf65fbba
Joonmo Yang <dev@remagpie.com>
2020-05-20 01:10:05 +0900

    Add bindings for undo tree

--- packages/misc/undo-tree.el
+++ packages/misc/undo-tree.el
@@ -1,5 +1,11 @@
 (require 'req-package)
+(require 'moderator)
 
 (req-package undo-tree
   :config
+  (setq undo-tree-map (make-sparse-keymap))
+  ;; (setq undo-tree-visualizer-mode-map (make-sparse-keymap))
+  ;; (setq undo-tree-visualizer-selection-mode-map (make-sparse-keymap))
+  (define-key undo-tree-map [remap undo] 'undo-tree-undo)
+  (define-key undo-tree-map [remap redo] 'undo-tree-redo)
   (global-undo-tree-mode t))
